433a4db7053e6802550d6aee553a28edd9d5450a
Cristian Stügelmayer <cristian@yellowspot.dev>
2024-06-22 15:12:10 -0300

    add Express and dot env

---
+++ app.js
@@ -0,0 +1,10 @@
+import 'dotenv/config';
+import express from 'express';
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log('Listening on port', PORT);
+});
