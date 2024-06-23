152e77d7c5d82dc2842f02e107015f43adafcfd5
Cristian Stügelmayer <cristian@yellowspot.dev>
2024-06-23 15:16:43 -0300

    Change descriptions

--- commands/commands.js
+++ commands/commands.js
@@ -6,22 +6,30 @@ import { APP_ID } from '../constants.js';
 // Simple test command
 const TEST_COMMAND = {
   name: 'test',
-  description: 'Basic command',
+  description: 'Basic command, show a random emoji',
   type: 1,
 };
 
 const QUESTION_COMMAND = {
   name: 'pregunta',
-  description: 'Trivia question',
+  description: 'Crea una pregunta trivia para que todos puedan contestar',
   type: 1,
 };
 
 const POLL_COMMAND = {
   name: 'encuesta',
-  description: 'Trivia question',
+  description: 'Crea una encuesta para que todos puedan votar',
   type: 1,
 };
 
 const ALL_COMMANDS = [QUESTION_COMMAND, POLL_COMMAND];
+const DEV_COMMANDS = [TEST_COMMAND, ...ALL_COMMANDS];
 
-installGlobalCommands(APP_ID, ALL_COMMANDS);
+if (process.env.NODE_ENV === 'development') {
+  console.log('Installing dev commands');
+  installGlobalCommands(APP_ID, DEV_COMMANDS);
+} else {
+  console.log('Installing all commands');
+  installGlobalCommands(APP_ID, ALL_COMMANDS);
+}
+
